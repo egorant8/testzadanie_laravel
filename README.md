be3288236c41914fdddbc821d89893ade45b8863
egorant8 <51228266+egorant8@users.noreply.github.com>
2022-05-13 19:40:46 +0300

    Вывод разделов + фото
    Добавлен в Readme пример JSON-а, почти дописан контроллер.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # testzadanie_laravel
 using php8.0 + apache 2.4-php8.0
+
+ПРИМЕР JSON
+{"all":{"razdel":[{"name":"ИМЯ 1 РАЗДЕЛА","photo":[{"url_photo":"ССЫЛКА НА 1 ФОТО 1го раздела"},{"url_photo":"ССЫЛКА НА 2 ФОТО 1го раздела"}]},{"name":"ИМЯ 2 РАЗДЕЛА","photo":[{"url_photo":"ССЫЛКА НА 1 ФОТО 2го раздела"}]}]}}
+И т.д.
--- app/Http/Controllers/json_controller.php
+++ app/Http/Controllers/json_controller.php
@@ -6,11 +6,29 @@ use Illuminate\Http\Request;
 
 class json_controller extends Controller
 {
-    public  function    checkJson(Request $reg)
+    public function checkJson(Request $reg)
     {
-        if (    $reg['json'] != "")
-        {
-            return  "Генерирую код";
-        }
+            if(!isset($reg['json']) || !$reg['json']){
+                return json_encode(['status'=>'error']);
+            }
+            $count_podrazdelov = "";
+            $data = json_decode($reg['json'], true);
+            echo '<!DOCTYPE html><html><head><meta charset="utf-8"><title></title></head><body>';
+            echo '<ul>';
+            foreach($data['all']['razdel'] as $item){
+                echo '<li>';
+                echo $item['name'];
+                echo '<ul>';
+                foreach($item['photo'] as $photo)
+                {
+                    echo '<li>';
+                    echo file_exists($photo['url_photo']) ? '<img src="'. $photo['url_photo'] .'" />' : 'Ошибка обработки фотографии. Файл не найден.';
+                    echo '</li>';
+                }
+                echo '</ul>';
+                echo '</li>';
+            }
+            echo '</ul>';
+            echo '</body></html>';
     }
 }
