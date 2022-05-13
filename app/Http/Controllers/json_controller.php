691a91826d394add7b849d1fcf4bb26707a083da
egorant8 <51228266+egorant8@users.noreply.github.com>
2022-05-13 20:15:38 +0300

    Update json_controller.php

--- app/Http/Controllers/json_controller.php
+++ app/Http/Controllers/json_controller.php
@@ -11,9 +11,25 @@ class json_controller extends Controller
             if(!isset($reg['json']) || !$reg['json']){
                 return json_encode(['status'=>'error']);
             }
-            $count_podrazdelov = "";
+            $depth = 1;
+            if(isset($reg['depth']))
+            {
+                $depth = $reg['depth'];
+            }
+            $count_podrazdelov = 1;
             $data = json_decode($reg['json'], true);
-            echo '<!DOCTYPE html><html><head><meta charset="utf-8"><title></title></head><body>';
+            echo '<!DOCTYPE html><html><head><meta charset="utf-8"><title></title></head>';
+            if(isset($reg['background']))
+            {
+                $background = $reg['background'];
+
+                echo '<body style="background: url('.$background.'); background-color:RGB'.$background.';">';
+            }
+            else
+            {
+                echo '<body>';
+            }
+
             echo '<ul>';
             foreach($data['all']['razdel'] as $item){
                 echo '<li>';
@@ -21,9 +37,29 @@ class json_controller extends Controller
                 echo '<ul>';
                 foreach($item['photo'] as $photo)
                 {
-                    echo '<li>';
-                    echo file_exists($photo['url_photo']) ? '<img src="'. $photo['url_photo'] .'" />' : 'Ошибка обработки фотографии. Файл не найден.';
-                    echo '</li>';
+                    if (mb_strtolower($reg['depth']) == "max") {
+                        echo '<li>';
+                        echo file_exists($photo['url_photo']) ? '<img src="'. $photo['url_photo'] .'" />' : 'Ошибка обработки фотографии. Файл не найден.';
+                        echo '</li>';
+                    }
+                    elseif ($depth >= 2) {
+                        echo '<li>';
+                        if ($depth != $count_podrazdelov) {
+                            echo file_exists($photo['url_photo']) ? '<img src="'. $photo['url_photo'] .'" />' : 'Ошибка обработки фотографии. Файл не найден.';
+                        }
+                        else
+                        {
+
+                        }
+                        echo '</li>';
+                    }
+                    elseif ($depth == 1) {
+                        echo '<li>';
+                        echo file_exists($photo['url_photo']) ? '<img src="'. $photo['url_photo'] .'" />' : 'Ошибка обработки фотографии. Файл не найден.';
+                        echo '</li>';
+                        break;
+                    }
+                    $count_podrazdelov++;
                 }
                 echo '</ul>';
                 echo '</li>';
